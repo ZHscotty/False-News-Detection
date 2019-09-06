c557e5aa6a9b9e3c603bdb1bcb69ef1e399319a7
ZHscotty <z1zhaohang@163.com>
2019-09-06 18:02:23 +0800

    增加测试集的id

--- code/data.py
+++ code/data.py
@@ -11,7 +11,7 @@ class Data:
     def __init__(self, train_path=None, test_path=None):
         self.train_path = train_path
         self.test_path = test_path
-        self.text_train, self.label, self.text_test = self.load_data()
+        self.text_train, self.label, self.text_test, self.text_id = self.load_data()
         self.train_id, self.label, self.test_id, self.word2index = self.data_process(self.text_train, self.label, self.text_test)
         self.index2word = self.index2word(self.word2index)
 
@@ -21,7 +21,8 @@ class Data:
         text_train = train_data['text'].tolist()
         label = train_data['label'].tolist()
         text_test = test_data['text'].tolist()
-        return text_train, label, text_test
+        text_id = test_data['id'].tolist()
+        return text_train, label, text_test, text_id
 
     #分词，并去掉标点符号
     def segement(self, data):
@@ -66,24 +67,7 @@ class Data:
         word2index = self.word2index(words)
         train_id = self.word2id(x_seg, word2index)
         test_id = self.word2id(test_seg, word2index)
-        test_id = pad_sequences(test_id, maxlen=300)
-        train_id = pad_sequences(train_id, maxlen=300)
+        test_id = pad_sequences(test_id, maxlen=300, padding='post', truncating='post')
+        train_id = pad_sequences(train_id, maxlen=300, padding='post', truncating='post')
         label = to_categorical(label, num_classes=2)
         return train_id, label, test_id, word2index
-
-
-
-
-
-# if __name__ == '__main__':
-#     path = '../data/train.csv'
-#     path2 = '../data/test_stage1.csv'
-#     data = Data(train_path=path, test_path=path2)
-#     index2word = data.index2word
-#     test_train = data.text_train[0]
-#     print(test_train)
-#     data_str = []
-#     for x in data.train_id[0]:
-#         if index2word[x] != 'PAD':
-#             data_str.append(index2word[x])
-#     print(data_str)
